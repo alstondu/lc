a086c7e10d060a804579b5a403e0c82c0d72ad15
alstondu <dudu2545874349@gmail.com>
2024-05-27 22:54:47 +0100

    Added header file

---
+++ include/utils.h
@@ -0,0 +1,24 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include<iostream>
+#include <vector>
+using namespace std;
+
+// Listnode
+struct ListNode {
+    int val;  // 节点上存储的元素
+    ListNode *next;  // 指向下一个节点的指针
+    ListNode(int x) : val(x), next(NULL) {}  // 节点的构造函数
+};
+
+
+void displayMatrix(const std::vector<std::vector<int>>& matrix) {
+    for (const auto& row : matrix) { // Iterate through each row
+        for (const auto& element : row) { // Iterate through each element in the row
+            std::cout << element << " "; // Print element followed by a space
+        }
+        std::cout << std::endl; // After each row, print a newline
+    }
+}
+
+#endif
