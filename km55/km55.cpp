b85e20ed484297fa2cd90d8d878a961111c29762
alstondu <dudu2545874349@gmail.com>
2024-06-01 15:53:39 +0100

    km55.右旋字符串 P1

---
+++ km55/km55.cpp
@@ -0,0 +1,27 @@
+//  Created by Yuang Du on 2024/05/29.
+//  km54. 右旋字符串
+
+#include "lc.h"
+
+class Solution {
+public:
+    void reverse(string& s, int start, int end){
+        for (int i = start, j = end; i < j; i++, j--){
+            swap(s[i], s[j]);
+        }
+    }
+
+    string rotateStr(string s, int k) {
+        reverse(s, 0, s.size() - 1);
+        reverse(s, 0, k);
+        reverse(s, k, s.size() - 1);
+        return s;
+    }
+};
+
+int main(){
+    string s = "abcdefg";
+    Solution solution;
+    cout << solution.rotateStr(s, 2) << endl;
+    return 0;
+}
