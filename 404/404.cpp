87c82aad8c9abb2663a82b7d9e9a157cbfef3723
alstondu <dudu2545874349@gmail.com>
2024-06-13 20:54:17 +0100

    Lc 404.左叶子之和 P1

---
+++ 404/404.cpp
@@ -0,0 +1,23 @@
+//  Created by Yuang Du on 2024/06/6.
+//  404. 左叶子之和
+
+#include "lc.h"
+
+class Solution {
+public:
+    int result = 0;
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root == NULL) return 0;
+        if(root->left == NULL && root->right == NULL) return 0;
+        if(root->left != NULL && root->left->left == NULL && root->left->right == NULL) {
+            result += root->left->val;
+        }
+        sumOfLeftLeaves(root->left);
+        sumOfLeftLeaves(root->right);
+        return result;
+    }
+};
+
+int main(){
+    return 0;
+}
