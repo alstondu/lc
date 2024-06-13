48e1222df50b297a7a4a0183b18ae92e106b4745
alstondu <dudu2545874349@gmail.com>
2024-06-13 20:52:52 +0100

    Lc 完全二叉树的节点个数 P1

---
+++ 222/222.cpp
@@ -0,0 +1,31 @@
+//  Created by Yuang Du on 2024/06/6.
+//  222. 完全二叉树节点的数量
+
+#include "lc.h"
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL) return 0;
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        int leftD = 0, rightD = 0;
+        while(left){
+            left = left->left;
+            leftD++;
+        }
+        while(right){
+            right = right->right;
+            rightD++;
+        }
+        if (leftD == rightD) return (2<<leftD) - 1;
+
+        int leftNum = countNodes(root->left);
+        int rightNum = countNodes(root->right);
+        return leftNum + rightNum + 1;
+    }
+};
+
+int main (){
+    return 0;
+}
