4752f98e4f5cd72268c84a2358fbf76cf5bd0a89
alstondu <dudu2545874349@gmail.com>
2024-06-14 10:34:00 +0100

    Lc 106, 105.构造二叉树 P1

---
+++ 106, 105/105.cpp
@@ -0,0 +1,15 @@
+//  Created by Yuang Du on 2024/06/7.
+//  105. 从前序与中序遍历序列构造二叉树
+
+#include "lc.h"
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+    }
+};
+
+int main (){
+    return 0;
+}
---
+++ 106, 105/106.cpp
@@ -0,0 +1,15 @@
+//  Created by Yuang Du on 2024/06/7.
+//  106. 从中序与后序遍历序列构造二叉树
+
+#include "lc.h"
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+
+    }
+};
+
+int main (){
+    return 0;
+}
