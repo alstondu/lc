aa09f4cfb4195a88bf758d7f4d603edd91e142c6
alstondu <dudu2545874349@gmail.com>
2024-06-14 10:33:17 +0100

    Lc 513.找树左下角的值 P1

---
+++ 513/513.cpp
@@ -0,0 +1,15 @@
+//  Created by Yuang Du on 2024/06/7.
+//  513. 找树左下角的值
+
+#include "lc.h"
+
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+
+    }
+};
+
+int main (){
+    return 0;
+}
