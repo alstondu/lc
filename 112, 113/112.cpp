b8975b8e9ec2a55b66dcf672c318a24e0668b86f
alstondu <dudu2545874349@gmail.com>
2024-06-14 10:34:42 +0100

    Lc 112, 113.路径总和 P1

---
+++ 112, 113/112.cpp
@@ -0,0 +1,15 @@
+//  Created by Yuang Du on 2024/06/7.
+//  112. 路径总和
+
+#include "lc.h"
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+
+    }
+};
+
+int main (){
+    return 0;
+}
---
+++ 112, 113/113.cpp
@@ -0,0 +1,15 @@
+//  Created by Yuang Du on 2024/06/7.
+//  113. 路径总和 II 
+
+#include "lc.h"
+
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+
+    }
+};
+
+int main (){
+    return 0;
+}
