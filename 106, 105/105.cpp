6715754477a0024f1c5d6244eee729faae3e5ec0
alstondu <dudu2545874349@gmail.com>
2024-06-15 13:19:25 +0100

    Lc 106, 105.构造二叉树 P1

--- 106, 105/105.cpp
+++ 106, 105/105.cpp
@@ -6,7 +6,27 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.size() == 0) return NULL;
 
+        int rootValue = preorder[0];
+        TreeNode* root = new TreeNode(rootValue);
+        if (preorder.size() == 1) return root;
+
+        int index;
+        for(index = 0; index < inorder.size(); index++){
+            if (inorder[index] == rootValue) break;
+        }
+
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + index);
+        vector<int> rightInorder(inorder.begin() + index + 1, inorder.end());
+        // preorder.resize(preorder.size() - 1);
+        vector<int> leftPreorder(preorder.begin() + 1, preorder.begin() + index + 1);
+        vector<int> rightPreorder(preorder.begin() + index + 1, preorder.end());
+
+        root->left = buildTree(leftPreorder, leftInorder);
+        root->right = buildTree(rightPreorder, rightInorder);
+
+        return root;
     }
 };
 
--- 106, 105/106.cpp
+++ 106, 105/106.cpp
@@ -6,7 +6,27 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (postorder.size() == 0) return NULL;
 
+        int rootValue = postorder[postorder.size() - 1];
+        TreeNode* root = new TreeNode(rootValue);
+        if (postorder.size() == 1) return root; 
+
+        int index;
+        for (index = 0; index < inorder.size(); index++){
+            if (inorder[index] == rootValue) break;
+        }
+
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + index);
+        vector<int> rightInorder(inorder.begin() + index + 1, inorder.end());
+        // postorder.resize(postorder.size() - 1);
+        vector<int> leftPostorder(postorder.begin(), postorder.begin() + index);
+        vector<int> rightPostorder(postorder.begin() + index, postorder.end() - 1);
+
+        root->left = buildTree(leftInorder, leftPostorder);
+        root->right = buildTree(rightInorder, rightPostorder);
+
+        return root;
     }
 };
 
